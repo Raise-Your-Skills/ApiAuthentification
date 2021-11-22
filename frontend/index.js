2add24b40d04cc9f2f0a0fea96d528a110afa3cb
Sebastien Colbert <colbertsebastien@gmail.com>
2021-11-22 11:28:24 +0100

    SignUp Fonctionnel

--- frontend/index.js
+++ frontend/index.js
@@ -13,20 +13,76 @@ document.querySelector(".back").addEventListener('click', () => {
 	document.querySelector(".signUp").classList.remove("inactive-sx");
 });
 
+// Affichage sign ou account
+if (localStorage.getItem('token')) {
+  document.getElementById('sign').style.display = 'none'
+  const account = document.getElementById('account')
+  account.style.display = 'none'
+  account.innerHTML = formTpl()
+} else {
+  document.getElementById('sign').style.display = 'flex'
+}
+
+//
+// Config
+//
+
+const apiUrl = 'http://localhost:5000'
+console.log('--> URL API')
+console.log(apiUrl)
 
 //
 // Functions
 //
 
-// Account page
+// Se connecter
+function sendSignIn () {
+  const signIn = document.getElementById("signIn")
+  signIn.addEventListener('submit', e => {
+    e.preventDefault()
+
+    // console.log(signIn)
+
+    let submitSignIn = {}
+    Array.from(new FormData(signIn), entry => submitSignIn[entry[0]] = entry[1])
+    // Array.from(new FormData(signIn), entry => console.log(entry))
+
+    // console.log(submitSignIn)
+
+    /* J'envoie les données à l'API et je récupére la réponse */
+    fetch(`${apiUrl}/api/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(submitSignIn)
+    })
+    .then(response => {
+      document.getElementById('responseContainer')
+              .innerHTML = `<pre>Status: ${response.status}</pre>`
+      const infoStatus = response.status === 200 ? 'Email Valide' : 'Email inconnu'
+      document.getElementById('infoLogin')
+              .innerHTML += `<p>${infoStatus}</p>`
+      if(response.ok){
+        // console.log(response)
+        response.json().then(data => {
+          // console.log(data)
+          localStorage.setItem('token', data.token)
+          document.getElementById('responseContainer')
+                  .innerHTML += `<pre>${JSON.stringify(data, null, 2)}</pre>`
+        })
+      }
+    })
+  })
+}
+
+// Mise à jour des infos utilisateur
 function formTpl() {
-  let useMethod = 'POST'
   let id = ``
   let avatarImg = 'avatar_default.jpg'
   let avatar = ``
   let first_name = ``
   let last_name = ``
   let email = ``
+  let bio = ``
 
   // if (user != '') {
   //   useMethod = 'PUT'
@@ -39,8 +95,12 @@ function formTpl() {
   // }
 
   return `
+
     <form id="addForm" method="POST">
-      <p class="col-12" id="msg"></p>
+      <div class="col-12">
+        <h2>Profil</h2>
+        <p id="msg"></p>
+      </div>
       <div class="col-6">
         <div id="img-preview"><img src="images/${avatarImg}" /></div>
         <label for="avatar">Choisissez votre Avatar</label>
@@ -51,8 +111,8 @@ function formTpl() {
         <input type="text" name="first_name" id="first_name" placeholder="Prénom" ${first_name} required>
         <input type="text" name="last_name" id="last_name" placeholder="Nom" ${last_name} required>
         <input type="email" name="email" id="email" placeholder="E-Mail" ${email} required>
-        <textarea name="bio" id="bio" cols="30" rows="10"></textarea>
-        <input type="submit" onClick="sendForm('${useMethod}')" value="Enregistrer">
+        <textarea name="bio" id="bio" cols="30" rows="10" placeholder="Bio">${bio}</textarea>
+        <input type="submit" onClick="sendForm()" value="Enregistrer">
       </div>
     </form>
   `
@@ -75,13 +135,6 @@ function avatarPreview () {
   })
 }
 
-// const sign = document.getElementById('sign')
-// sign.style.display = 'none'
-
-const account = document.getElementById('account')
-// account.style.display = 'none'
-account.innerHTML = formTpl()
-
 // Update
 function updateUser (userId) {
   const user = {
@@ -101,16 +154,77 @@ function updateUser (userId) {
 }
 
 // Delete
-// function deleteUser (userId) {
-//   // console.log(`https://reqres.in/api/users/${userId}`)
-//   fetch(`https://reqres.in/api/users/${userId}`, { method: 'DELETE' })
-//   .then(response => {
-//     let del = ''
-//     if(response.ok){
-//       document.getElementById(`id-${userId}`).style.display = 'none'
-//       del = `, User #${userId} exterminate !`
-//     }
-//     document.getElementById('responseContainer')
-//             .insertAdjacentHTML('afterbegin', `<pre>Status: ${response.status}${del}</pre>`)
-//   })
-// }
+function deleteUser (userId) {
+  // console.log(`${apiUrl}/api/users/${userId}`)
+  fetch(`${apiUrl}/api/users/${userId}`, { method: 'DELETE' })
+  .then(response => {
+    let del = ''
+    if(response.ok){
+      document.getElementById(`id-${userId}`).style.display = 'none'
+      del = `, User #${userId} exterminate !`
+    }
+    document.getElementById('responseContainer')
+            .insertAdjacentHTML('afterbegin', `<pre>Status: ${response.status}${del}</pre>`)
+  })
+}
+
+
+//
+// Les events
+//
+
+// Création d'un user
+const signUp = document.getElementById("signUp")
+signUp.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log('--> SignUp')
+
+  // Je récupére les entrées
+  let submitSignUp = {}
+  Array.from(new FormData(signUp), (entry) => {
+    if (entry[0] === 'avatar') {
+      submitSignUp[entry[0]] = entry[1].name
+    } else {
+      submitSignUp[entry[0]] = entry[1]
+    }
+  })
+
+  // console.log(submitSignUp)
+
+  /* J'envoie les données à l'API et je récupére la réponse */
+  let url = `${apiUrl}/api/users/signup`
+  // console.log(url)
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(submitSignUp)
+  })
+  .then(response => {
+    console.log(`Status: ${response.status}`)
+    if(response.ok) {
+      console.log(response)
+      response.json().then(data => {
+        console.log(data.message)
+        document.getElementById('message').innerHTML = `<p class="good">${data.message}</p>`
+        // On bascule sur le form de connection
+        document.querySelector(".signIn").classList.add("active-dx")
+        document.querySelector(".signUp").classList.add("inactive-sx")
+        document.querySelector(".signUp").classList.remove("active-sx")
+        document.querySelector(".signIn").classList.remove("inactive-dx")
+        // On vide les inputs
+        document.getElementById("signUp").email.value = ''
+        document.getElementById("signUp").password.value = ''
+        document.getElementById("signIn").email.value = ''
+      })
+    } else {
+      response.json().then(data => {
+        document.getElementById('message').innerHTML = `<p class="bad">${data.error}</p>`
+        console.log(data.error.errors)
+        if (data.error.errors) {
+          document.getElementById('message').innerHTML = `<p class="bad">Cette adresse email existe déjà !</p>`
+        }
+      })
+    }
+  })
+})
+
