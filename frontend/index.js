f0819d3c1162a687e61c611cf94ed3f807940b93
Sebastien Colbert <colbertsebastien@gmail.com>
2021-11-22 14:05:05 +0100

    SignIn & Exit Fonctionnel

--- frontend/index.js
+++ frontend/index.js
@@ -1,27 +1,31 @@
 // Animation formulaire Login/Signup
 document.querySelector(".log-in").addEventListener('click', () => {
-	document.querySelector(".signIn").classList.add("active-dx");
-	document.querySelector(".signUp").classList.add("inactive-sx");
-	document.querySelector(".signUp").classList.remove("active-sx");
-	document.querySelector(".signIn").classList.remove("inactive-dx");
-});
+	document.querySelector(".signIn").classList.add("active-dx")
+	document.querySelector(".signUp").classList.add("inactive-sx")
+	document.querySelector(".signUp").classList.remove("active-sx")
+	document.querySelector(".signIn").classList.remove("inactive-dx")
+})
 
 document.querySelector(".back").addEventListener('click', () => {
-	document.querySelector(".signUp").classList.add("active-sx");
-	document.querySelector(".signIn").classList.add("inactive-dx");
-	document.querySelector(".signIn").classList.remove("active-dx");
-	document.querySelector(".signUp").classList.remove("inactive-sx");
-});
+	document.querySelector(".signUp").classList.add("active-sx")
+	document.querySelector(".signIn").classList.add("inactive-dx")
+	document.querySelector(".signIn").classList.remove("active-dx")
+	document.querySelector(".signUp").classList.remove("inactive-sx")
+})
 
-// Affichage sign ou account
-if (localStorage.getItem('token')) {
-  document.getElementById('sign').style.display = 'none'
-  const account = document.getElementById('account')
-  account.style.display = 'none'
-  account.innerHTML = formTpl()
-} else {
-  document.getElementById('sign').style.display = 'flex'
+
+// Selection page
+function isAuth() {
+  if (localStorage.getItem('token')) {
+    document.getElementById('sign').style.display = 'none'
+    document.getElementById('account').style.display = 'flex'
+  } else {
+    document.getElementById('sign').style.display = 'flex'
+    document.getElementById('account').style.display = 'none'
+  }
 }
+isAuth()
+
 
 //
 // Config
@@ -34,90 +38,6 @@ console.log(apiUrl)
 //
 // Functions
 //
-
-// Se connecter
-function sendSignIn () {
-  const signIn = document.getElementById("signIn")
-  signIn.addEventListener('submit', e => {
-    e.preventDefault()
-
-    // console.log(signIn)
-
-    let submitSignIn = {}
-    Array.from(new FormData(signIn), entry => submitSignIn[entry[0]] = entry[1])
-    // Array.from(new FormData(signIn), entry => console.log(entry))
-
-    // console.log(submitSignIn)
-
-    /* J'envoie les données à l'API et je récupére la réponse */
-    fetch(`${apiUrl}/api/login`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(submitSignIn)
-    })
-    .then(response => {
-      document.getElementById('responseContainer')
-              .innerHTML = `<pre>Status: ${response.status}</pre>`
-      const infoStatus = response.status === 200 ? 'Email Valide' : 'Email inconnu'
-      document.getElementById('infoLogin')
-              .innerHTML += `<p>${infoStatus}</p>`
-      if(response.ok){
-        // console.log(response)
-        response.json().then(data => {
-          // console.log(data)
-          localStorage.setItem('token', data.token)
-          document.getElementById('responseContainer')
-                  .innerHTML += `<pre>${JSON.stringify(data, null, 2)}</pre>`
-        })
-      }
-    })
-  })
-}
-
-// Mise à jour des infos utilisateur
-function formTpl() {
-  let id = ``
-  let avatarImg = 'avatar_default.jpg'
-  let avatar = ``
-  let first_name = ``
-  let last_name = ``
-  let email = ``
-  let bio = ``
-
-  // if (user != '') {
-  //   useMethod = 'PUT'
-  //   id = `<input type="hidden" id="userId" name="userId" value="${user.id}">`
-  //   avatarImg = user.avatar
-  //   avatar = `value="${user.avatar}"`
-  //   first_name = `value="${user.first_name}"`
-  //   last_name = `value="${user.last_name}"`
-  //   email = `value="${user.email}"`
-  // }
-
-  return `
-
-    <form id="addForm" method="POST">
-      <div class="col-12">
-        <h2>Profil</h2>
-        <p id="msg"></p>
-      </div>
-      <div class="col-6">
-        <div id="img-preview"><img src="images/${avatarImg}" /></div>
-        <label for="avatar">Choisissez votre Avatar</label>
-        <input type="file" accept="image/*" name="avatar" id="avatar" ${avatar}">
-      </div>
-      <div class="col-6">
-        ${id}
-        <input type="text" name="first_name" id="first_name" placeholder="Prénom" ${first_name} required>
-        <input type="text" name="last_name" id="last_name" placeholder="Nom" ${last_name} required>
-        <input type="email" name="email" id="email" placeholder="E-Mail" ${email} required>
-        <textarea name="bio" id="bio" cols="30" rows="10" placeholder="Bio">${bio}</textarea>
-        <input type="submit" onClick="sendForm()" value="Enregistrer">
-      </div>
-    </form>
-  `
-}
-
 function avatarPreview () {
   const avatar = document.getElementById("avatar")
   const imgPreview = document.getElementById("img-preview")
@@ -173,7 +93,7 @@ function deleteUser (userId) {
 // Les events
 //
 
-// Création d'un user
+// SignUp
 const signUp = document.getElementById("signUp")
 signUp.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -228,3 +148,46 @@ signUp.addEventListener('submit', (e) => {
   })
 })
 
+// SignIn
+const signIn = document.getElementById("signIn")
+signIn.addEventListener('submit', e => {
+  e.preventDefault()
+
+  console.log('--> SignIn')
+
+  // Je récupére les entrées
+  let submitSignIn = {}
+  Array.from(new FormData(signIn), entry => {
+    submitSignIn[entry[0]] = entry[1]
+  })
+
+  // console.log(submitSignIn)
+
+  // J'envoie les données à l'API et je récupére la réponse
+  fetch(`${apiUrl}/api/users/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(submitSignIn)
+  })
+  .then(response => {
+    console.log(`Status: ${response.status}`)
+    if(response.ok){
+      response.json().then(data => {
+        localStorage.setItem('token', data.token)
+        isAuth()
+      })
+    } else {
+      response.json().then(data => {
+        console.log(data)
+        document.getElementById('message').innerHTML = `<p class="bad">${data.error}</p>`
+      })
+    }
+  })
+})
+
+// Exit
+const btnExit = document.getElementById("btnExit")
+btnExit.addEventListener('click', e => {
+  localStorage.removeItem('token')
+  isAuth()
+})
